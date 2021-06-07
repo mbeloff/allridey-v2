e3b4cd897c916056bd80f133fdfff418e3ea689b
michael beloff <michael.a.beloff@gmail.com>
2021-06-07 12:29:06 +1000

    optimise img

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -10,9 +10,9 @@
       <i class="fas fa-star text-yellow-400"></i>
     </span>
     <p class="text-xl mt-4 mb-4">Cheap car and camper rentals across Australia and New Zealand</p>
-      <router-link :to="{ name: 'Search' }" class="px-6 py-2 bg-blue-500 hover:bg-yellow-500 text-white rounded m-12 text-xl"><i class="fal fa-search"></i> Find a Vehicle </router-link>
+      <router-link :to="{ name: 'Search' }" class="px-6 py-2 bg-blue-500 hover:bg-yellow-500 text-white rounded m-12 text-xl"><i class="fal fa-search"></i> BOOK NOW </router-link>
     
-    <img class="max-w-screen-lg w-screen" src="https://res.cloudinary.com/dg5ybbkbh/image/upload/v1582172582/allridey/header2.png" alt="">
+    <img class="max-w-screen-lg w-screen" src="https://res.cloudinary.com/dg5ybbkbh/image/upload/c_fit,h_350/f_auto,q_auto/v1582172582/allridey/header2.png" alt="">
     </div>
    
     
