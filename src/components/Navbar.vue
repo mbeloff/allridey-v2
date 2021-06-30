0fe2993c8ea73a76e8cbb423033aa1acad84254d
michael beloff <michael.a.beloff@gmail.com>
2021-06-30 11:44:45 +1000

    style tweak

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="w-full h-20 shadow-lg bg-white z-10">
     <div class="flex justify-between container h-full mx-auto px-2"> 
       <div class="left flex content-center items-center gap-x-4">
-        <router-link to="/" class="left w-24 flex">
+        <router-link to="/" class="left w-16 flex">
       <img src="https://res.cloudinary.com/dg5ybbkbh/image/upload/v1582172019/allridey/allridey-logo.svg" class="object-contain" alt="">
     </router-link>
     <!-- <router-link class="nav-item" exact-active-class="text-blue-500" to="/">HOME</router-link> -->
@@ -10,7 +10,7 @@
       </div>
       
     <div class="right h-full flex place-items-center">
-      <a href="tel:1800246869" class="text-2xl font-bold text-blue-500">1800 24 68 69</a>
+      <a href="tel:1800246869" class="font-bold text-blue-600">1800 24 68 69</a>
     </div>
     </div>
     
