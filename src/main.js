7b098de3fd0174e967a10e713a73773294175aff
michael beloff <michael.a.beloff@gmail.com>
2021-07-16 09:32:12 +1000

    add vuex store

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import { DatePicker } from 'v-calendar';
+import store from "./store";
 
-createApp(App).use(router).component('DatePicker', DatePicker).mount('#app')
+createApp(App).use(router).use(store).component('DatePicker', DatePicker).mount('#app')
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {
+  createStore
+} from 'vuex'
+
+export default createStore({
+  state: {
+    phone: '1800 24 68 69',
+    phoneuri: '1800246869'
+  },
+})
