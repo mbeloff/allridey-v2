5bf7d0847779635909bd5959fae8324dd5471897
michael beloff <michael.a.beloff@gmail.com>
2021-07-16 14:57:38 +1000

    add component

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="absolute w-full h-full top-0 left-0 rounded grid place-items-center bg-gray-200 bg-opacity-80 no-pointer">
+    <spinner></spinner>
+  </div>
+</template>
+
+<script>
+import Spinner from './Spinner.vue'
+export default {
+  components: {Spinner}
+}
+</script>
+
+<style>
+.no-pointer {
+  pointer-events: none
+}
+</style>
