7523dcb583a9bb1a2fed9ee39124196af1e2f903
michael beloff <michael.a.beloff@gmail.com>
2021-07-19 15:10:16 +1000

    api test

--- netlify/functions/signRequest.js
+++ netlify/functions/signRequest.js
@@ -1,6 +1,7 @@
 const CryptoJS = require('crypto-js/crypto-js')
 const headers = {
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  // 'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Origin': 'https://dev.allridey.com.au/',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'POST'
 };
