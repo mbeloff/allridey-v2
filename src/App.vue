f4c1373bf31f730a1272711695964a30e37c0c6b
michael beloff <michael.a.beloff@gmail.com>
2021-07-23 15:05:42 +1000

    nil

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,63 @@
 <template>
-<div class="wrapper min-h-screen max-w-screen grid">
-  <Nav v-if="this.$route.name != 'CheckPayment'"/>
-  <div class="z-0">
-     <router-view />
+  <div class="wrapper min-h-screen max-w-screen grid">
+    <Nav v-if="this.$route.name != 'CheckPayment'" />
+    <div class="z-0">
+      <router-view />
+    </div>
+
+    <Footer v-if="this.$route.name != 'CheckPayment'" />
   </div>
-   
-  <Footer v-if="this.$route.name != 'CheckPayment'"/>
-</div>
 </template>
 
 <script>
-import Nav from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
-export default {
-  components: {
-    Nav, Footer
-  },
-}
+  import Nav from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
+  export default {
+    components: {
+      Nav,
+      Footer
+    },
+  }
 </script>
 
 <style lang="postcss">
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
-#app {
-  font-family: 'open Sans', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.wrapper {
-  display: grid;
-  grid-template-rows: auto 1fr auto
-}
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
 
-@layer components {
-.btn {
-  @apply px-4 py-2 font-bold rounded shadow
-}
-
-  .btn-primary {
-    @apply bg-gradient-to-tr from-blue-400 to-blue-600   text-white 
+  #app {
+    font-family: 'open Sans', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
 
-  .btn-primary:hover {
-    @apply from-blue-800 to-blue-900 ring-2 ring-blue-200
-  }
-  .btn-secondary {
-    @apply text-blue-600 rounded bg-gray-200 border shadow
+  .wrapper {
+    display: grid;
+    grid-template-rows: auto 1fr auto
   }
 
-  .btn-secondary:hover {
-    @apply bg-gradient-to-tr from-blue-500 to-blue-600 text-white
-  }
+  @layer components {
+    .btn {
+      @apply px-4 py-2 font-bold rounded shadow
+    }
 
-  .my-label {
+    .btn-primary {
+      @apply bg-gradient-to-tr from-blue-400 to-blue-600 text-white
+    }
+
+    .btn-primary:hover {
+      @apply from-blue-800 to-blue-900 ring-2 ring-blue-200
+    }
+
+    .btn-secondary {
+      @apply text-blue-600 rounded bg-gray-200 border shadow
+    }
+
+    .btn-secondary:hover {
+      @apply bg-gradient-to-tr from-blue-500 to-blue-600 text-white
+    }
+
+    .my-label {
       @apply text-blue-800 font-bold text-xs mb-0.5
     }
 
@@ -68,5 +72,5 @@ export default {
     .my-input {
       @apply h-8 border flex-1 pl-1 bg-gray-200 rounded
     }
-}
+  }
 </style>
