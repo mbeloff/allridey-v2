2859c5dc3351112c2d948768e8ddade2ed5da104
michael beloff <michael.a.beloff@gmail.com>
2021-08-03 15:41:08 +1000

    add basic search result filter/sort

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="text-left rounded w-full p-1 search-results">   
-    <p class="text-center" v-if="numAvailable > 0">search results</p>
-    <p class="text-center" v-else>No results found</p>
-    <div v-for="(cat, i) in getCats(categories)" class="my-2">
+  <div class="text-left rounded w-full search-results relative">   
+    <p class="text-center absolute inset-x-0 top-0"  v-if="numAvailable == 0">No results found</p>
+    <!-- <div v-for="(cat, i) in getCats(categories)" class="my-2"> -->
       <!-- <div class="flex bg-gray-600 text-white px-2">
         <p>{{cat.vehiclecategorytype}}</p>
       </div> -->
-      <search-result @select-vehicle="selectVehicle" v-for="(car, i) in getCars(cat.id)" :key="car.vehiclecategory" :data="car" :manfees="getFees(car.vehiclecategorytypeid, car.vehiclecategoryid)" :allData="step2" >
+      <!-- <search-result @select-vehicle="selectVehicle" v-for="(car, i) in getCars(cat.id)" :key="car.vehiclecategory" :data="car" :manfees="getFees(car.vehiclecategorytypeid, car.vehiclecategoryid)" :allData="step2" >
       </search-result>
+    </div> -->
+    <div v-if="filteredVehicles.length" class="text-right">
+      <label class="text-sm mr-2" for="sort">sort by:</label>
+      <select class="my-input" name="sort" id="sort" v-model="filter">
+        <option value="default">default</option>
+        <option value="pricelow">lowest price</option>
+        <option value="pricehigh">highest price</option>
+      </select>
     </div>
+    <search-result @select-vehicle="selectVehicle" v-for="(car, i) in filteredVehicles" :key="car.vehiclecategory" :data="car" :manfees="getFees(car.vehiclecategorytypeid, car.vehiclecategoryid)" :allData="step2" >
+      </search-result>
   </div>
 </template>
 <script>
   import LoadingOverlay from './LoadingOverlay.vue'
   import SearchResult from './SearchResult.vue'
+  
   export default {
+    
     components: {
       SearchResult,
       LoadingOverlay
@@ -23,6 +34,7 @@
       return {
         categories: [],
         loading: false,
+        filter: "default"
       }
     },
     mounted() {
@@ -39,6 +51,15 @@
       this.$emit('mounted')      
     },
     computed: {
+      filteredVehicles() {
+        const sorters = {
+          // default doesn't do anything
+          default: () => {},
+          pricelow: (a,b) => this.calcEstimate(a) - this.calcEstimate(b),
+          pricehigh: (a,b) => this.calcEstimate(b) - this.calcEstimate(a),
+        };
+        return this.step2.availablecars.filter(car => car.availablemessage == 'Available for booking').slice().sort(sorters[this.filter]);
+      },
       step2() {
         return this.$store.state.step2
       },
@@ -61,26 +82,36 @@
       selectVehicle(data, step) {
         this.$emit('select-vehicle', data, step)
       },
-      getCats(catids) {
-        let arr = []
-        let all = this.step2.categorytypes
-        catids.forEach(function (el) {
-          all.forEach(function (x) {
-            if (x.id == el) {
-              arr.push(x)
-            }
-          })
-        })
-        return arr
-      },
-      getCars(categorytypeid) {
-        let arr = []
-        this.step2.availablecars.forEach(function (el) {
-          if (el.vehiclecategorytypeid == categorytypeid && el.available) {
-            arr.push(el)
+      // getCats(catids) {
+      //   let arr = []
+      //   let all = this.step2.categorytypes
+      //   catids.forEach(function (el) {
+      //     all.forEach(function (x) {
+      //       if (x.id == el) {
+      //         arr.push(x)
+      //       }
+      //     })
+      //   })
+      //   return arr
+      // },
+      // getCars(categorytypeid) {
+      //   let arr = []
+      //   this.step2.availablecars.forEach(function (el) {
+      //     if (el.vehiclecategorytypeid == categorytypeid && el.available) {
+      //       arr.push(el)
+      //     }
+      //   })
+      //   return arr
+      // },
+      calcEstimate(car) {
+        let totalRate = car.totalrateafterdiscount
+        let totalFees = 0
+        this.step2.mandatoryfees.forEach(function(el) {
+          if (el.vehiclecategorytypeid == car.vehiclecategorytypeid && el.vehiclecategoryid == car.vehiclecategoryid) {
+            totalFees += el.totalfeeamount
           }
         })
-        return arr
+        return totalRate + totalFees
       },
       getFees(categorytypeid, categoryid) {
         let arr = []
