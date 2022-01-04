b83651dbeade75ccc211b030035e91b517ebbcc3
michael beloff <michael.a.beloff@gmail.com>
2022-01-04 14:35:38 +1000

    add sale image

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -14,10 +14,10 @@
       <p>Allridey offers great value car and van rentals in Hobart. We've got the best prices - get an instant quote and book online today.</p>
     </div>
   </div>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-x-0 gap-y-10 md:gap-10">
-  <div v-for="(article, index) in articles" :key="index" class="w-full text-left rounded" :class="'md:col-span-' + article.cols">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-x-0 gap-y-10 md:gap-10">    
+  <div v-for="(article, index) in articles" :key="index" class="w-full text-left rounded" :class="article.cols">
     <div class="bg-white rounded shadow-lg group">
-       <div class="h-48 rounded-t bg-cover bg-bottom relative" :style="{'background-image' : 'url(' + article.img + ')'}">
+       <div class="h-48 rounded-t bg-cover bg-center relative" :style="{'background-image' : 'url(' + article.img + ')'}">
          <div class="absolute opacity-50 group-hover:opacity-100 transition ease-out bg-gradient-to-r h-full w-full rounded-t transtiion-colors" :class="'from-' + article.color + '-400'">           
          </div>
          <div class="absolute h-full w-full">
@@ -52,11 +52,18 @@
     data() {
       return {
         gallery: [],
-        articles: [{
+        articles: [
+          {
+            title: "Sale on Now",
+            content: "Get 50% off the daily rate for all hires.",
+            img: "https://res.cloudinary.com/allridey/image/upload/v1641269764/assets/allridey_sale.jpg",
+            cols: "md:col-span-1",
+          },
+          {
             title: "Car rental. Simple",
             content: "Get an instant quote and quickly book online. Cheap car and van rental in Hobart",
             img: "https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_scale,w_700/v1628040145/allridey/ajddSjc.jpg",
-            cols: "3",
+            cols: "md:col-span-2",
             linktext: "Book Now",
             linksto: "Search",
             scroll: "search-form",
@@ -67,16 +74,16 @@
             title: "Our fleet",
             content: "We have a range of economical cars and 8 seat vans at great daily rates. If you find a cheaper price, we'll beat it by 10%.",
             img: "https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_scale,w_700/v1627347849/allridey/jbkad.jpg",
-            cols: "2",
+            cols: "md:col-span-2",
             // linksto: "Search",
             color: 'yellow',
             overlayimg: 'https://res.cloudinary.com/allridey/image/upload/c_scale,w_500/q_auto,f_auto/v1640042972/assets/allridey-micra.png'
           },
           {
             title: "Roadside Assist",
-            content: "We're only a phone call away with 24 hour support and roadside assist included on all hires.",
+            content: "We're only a phone call away with 24 hour support and roadside assist included with your hire.",
             img: "https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_scale,w_700/v1627348031/allridey/Kdbad2.jpg",
-            cols: "1",
+            cols: "md:col-span-1",
             overlaytext: '<i class="fal fa-user-headset tranform rotate-3"></i>',
             // linksto: "Search",
             color: 'green'
