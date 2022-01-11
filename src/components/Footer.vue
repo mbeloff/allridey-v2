e487b3b5059aca1f64a05068b7c361f34733e9cf
michael beloff <michael.a.beloff@gmail.com>
2022-01-11 12:28:25 +1000

    layout tweaks

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,8 +5,8 @@
         <router-link v-for="location in locations" :key="location.city" class="hover:underline" :to="{path: '/location/' + location.city.toLowerCase().replace(/\s+/g, '-')}" >Car Hire {{location.city}}</router-link>
       </div>
       <div class="gap-2 text-sm text-left flex flex-col">
-        <a class="hover:underline" href="/ALLRIDEY-vra.pdf" download><i class=" far fa-file-pdf mr-2"></i>Terms and Conditions</a>
-        <router-link class="hover:underline" :to="'privacy'" download><i class=" far fa-file-pdf mr-2"></i>Privacy Policy</router-link>
+        <a class="hover:underline" href="/ALLRIDEY-vra.pdf" download><i class=" far fa-file-pdf mr-2 text-blue-500"></i>Terms and Conditions</a>
+        <router-link class="hover:underline" :to="'privacy'" download><i class=" far fa-file-pdf mr-2 text-blue-500"></i>Privacy Policy</router-link>
       </div>
 
     </div>
@@ -15,8 +15,9 @@
 
 
     <div>
-      <a :href="`mailto:` + this.$store.state.email"><i class="far fa-envelope mr-2"></i>{{this.$store.state.email}}</a>
-      <p class="text-gray-500 text-xs self-end">© Allridey Pty Ltd | ACN:
+      <a :href="`mailto:` + this.$store.state.email"><i class="far fa-envelope mr-2 text-blue-500"></i>{{this.$store.state.email}}</a>
+      <a href="/" class="left flex"><img src="/src/assets/allridey_text_outline.svg" alt="Go to Allridey homepage" class="flex-grow w-32 md:w-48 py-5"></a>
+      <p class="text-gray-500 text-xs self-end pb-2">© Allridey Pty Ltd | ACN:
         652772980</p>
     </div>
 
