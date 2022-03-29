4ecedd2feff659e96d09a7eb68f13c92c536895b
michael beloff <michael.a.beloff@gmail.com>
2022-03-29 10:47:51 +1000

    fix: merge made a big mess

--- src/components/forms/FormPaymentNew.vue
+++ src/components/forms/FormPaymentNew.vue
@@ -68,7 +68,6 @@ export default {
       deep: true,
     },
   },
-
   mounted() {
     this.getBookingInfo()
     window.addEventListener(
@@ -126,149 +125,126 @@ export default {
       let day = dateStr.substring(6, 8)
       return day + '/' + month + '/' + year
     },
-    watch: {
-      paymentResponse: 'handlePayment',
-      confirmedPayment: {
-        handler() {
-          console.log('payment confirmed')
-          this.trackPayment(this.bookinginfo)
-          this.sendEmail()
-          this.$router.push({
-            path: 'summary?pymnt=success',
-            query: {
-              pymnt: 'success',
-            },
-          })
-        },
-        cardExpiry(dateStr) {
-          let month = dateStr.substring(0, 2)
-          let year = dateStr.substring(2, 4)
-          return month + '/' + year
-        },
 
-        trackPayment() {
-          let items = [
-            {
-              item_name: this.bookinginfo.bookinginfo[0].vehiclecategory,
-              price: this.bookinginfo.rateinfo[0].ratesubtotal,
-              quantity: 1,
-              discount:
-                this.bookinginfo.rateinfo[0].dailyratebeforediscount *
-                this.bookinginfo.rateinfo[0].numberofdays,
-            },
-            {
-              item_name: this.bookinginfo.bookinginfo[0].kmcharges_description,
-              price:
-                this.bookinginfo.bookinginfo[0].kmcharges_totalfordailyrate,
-              quantity: 1,
-            },
-          ]
-          this.bookinginfo.extrafees.forEach((fee) => {
-            items.push({
-              item_name: fee.name,
-              price: fee.totalfeeamount,
-              quantity: fee.qty,
-            })
-          })
-          this.$gtag.event('purchase', {
-            currency: 'AUD',
-            event_category: 'ecommerce',
-            transaction_id:
-              this.bookinginfo.bookinginfo[0].reservationdocumentno,
-            value: this.bookinginfo.bookinginfo[0].totalcost,
-            items: items,
-            coupon: this.bookinginfo.rateinfo[0].discountname.replaceAll(
-              ' ',
-              '_'
-            ),
-          })
-        },
+    cardExpiry(dateStr) {
+      let month = dateStr.substring(0, 2)
+      let year = dateStr.substring(2, 4)
+      return month + '/' + year
+    },
 
-        gatherParams() {
-          let obj = this.paymentResponse
-          let params = {
-            method: 'confirmpayment',
-            reservationref:
-              this.$store.state.bookinginfo.bookinginfo[0].reservationref,
-            amount: obj.AmountSettlement._text,
-            success: obj.Success._text,
-            paytype: obj.CardName._text,
-            paydate: this.getPaydate(obj.DateSettlement._text),
-            supplierid: 2,
-            transactid: obj.TxnId._text,
-            dpstxnref: obj.DpsTxnRef._text,
-            cardholder: obj.CardHolderName._text,
-            paysource: 'Windcave Online Payment',
-            cardnumber: obj.CardNumber._text,
-            cardexpiry: this.cardExpiry(obj.DateExpiry._text),
-            transtype: 'Payment',
-            payscenario: 1,
-            emailoption: 1,
-          }
-          return params
+    trackPayment() {
+      let items = [
+        {
+          item_name: this.bookinginfo.bookinginfo[0].vehiclecategory,
+          price: this.bookinginfo.rateinfo[0].ratesubtotal,
+          quantity: 1,
+          discount:
+            this.bookinginfo.rateinfo[0].dailyratebeforediscount *
+            this.bookinginfo.rateinfo[0].numberofdays,
         },
+        {
+          item_name: this.bookinginfo.bookinginfo[0].kmcharges_description,
+          price: this.bookinginfo.bookinginfo[0].kmcharges_totalfordailyrate,
+          quantity: 1,
+        },
+      ]
+      this.bookinginfo.extrafees.forEach((fee) => {
+        items.push({
+          item_name: fee.name,
+          price: fee.totalfeeamount,
+          quantity: fee.qty,
+        })
+      })
+      this.$gtag.event('purchase', {
+        currency: 'AUD',
+        event_category: 'ecommerce',
+        transaction_id: this.bookinginfo.bookinginfo[0].reservationdocumentno,
+        value: this.bookinginfo.bookinginfo[0].totalcost,
+        items: items,
+        coupon: this.bookinginfo.rateinfo[0].discountname.replaceAll(' ', '_'),
+      })
+    },
 
-        getBookingInfo() {
-          let params = JSON.stringify({
-            method: 'bookinginfo',
-            reservationref: this.resinfo.reservationref,
-          })
-          Mixins.methods.apiCall(params).then((res) => {
-            this.$store.dispatch('bookinginfo', res)
-            this.requestWindcaveTransaction()
-          })
-        },
+    gatherParams() {
+      let obj = this.paymentResponse
+      let params = {
+        method: 'confirmpayment',
+        reservationref:
+          this.$store.state.bookinginfo.bookinginfo[0].reservationref,
+        amount: obj.AmountSettlement._text,
+        success: obj.Success._text,
+        paytype: obj.CardName._text,
+        paydate: this.getPaydate(obj.DateSettlement._text),
+        supplierid: 2,
+        transactid: obj.TxnId._text,
+        dpstxnref: obj.DpsTxnRef._text,
+        cardholder: obj.CardHolderName._text,
+        paysource: 'Windcave Online Payment',
+        cardnumber: obj.CardNumber._text,
+        cardexpiry: this.cardExpiry(obj.DateExpiry._text),
+        transtype: 'Payment',
+        payscenario: 1,
+        emailoption: 1,
+      }
+      return params
+    },
 
-        refreshBookingInfo() {
-          let params = JSON.stringify({
-            method: 'bookinginfo',
-            reservationref: this.resinfo.reservationref,
-          })
-          Mixins.methods.apiCall(params).then((res) => {
-            this.$store.dispatch('bookinginfo', res)
-            this.confirmedPayment = res
-          })
-        },
+    getBookingInfo() {
+      let params = JSON.stringify({
+        method: 'bookinginfo',
+        reservationref: this.resinfo.reservationref,
+      })
+      Mixins.methods.apiCall(params).then((res) => {
+        this.$store.dispatch('bookinginfo', res)
+        this.requestWindcaveTransaction()
+      })
+    },
 
-        requestWindcaveTransaction() {
-          let balancedue =
-            this.$store.state.bookinginfo.bookinginfo[0].balancedue
-          let currency =
-            this.$store.state.bookinginfo.bookinginfo[0].currencyname
-          let resref =
-            this.$store.state.bookinginfo.bookinginfo[0].reservationref
-          if (balancedue > 0) {
-            let Host = import.meta.env.VITE_HOST
-            var body = JSON.stringify({
-              amount: balancedue.toFixed(2),
-              currency: currency,
-              resref: resref,
-            })
-            var requestOptions = {
-              method: 'POST',
-              body: body,
-              redirect: 'follow',
-            }
-            fetch(Host + '/.netlify/functions/reqtrans', requestOptions)
-              .then((response) => response.text())
-              .then((result) => {
-                let res = JSON.parse(result).Request.URI._text
-                this.payurl = res
-                this.loading = false
-              })
-              .catch((error) => {
-                console.log('request transaction failed: ', error)
-              })
-          } else {
-            this.$router.push({
-              path: '/summary?pymnt=nobal',
-              query: {
-                pymnt: 'nobal',
-              },
-            })
-          }
-        },
-      },
+    refreshBookingInfo() {
+      let params = JSON.stringify({
+        method: 'bookinginfo',
+        reservationref: this.resinfo.reservationref,
+      })
+      Mixins.methods.apiCall(params).then((res) => {
+        this.$store.dispatch('bookinginfo', res)
+        this.confirmedPayment = res
+      })
+    },
+
+    requestWindcaveTransaction() {
+      let balancedue = this.$store.state.bookinginfo.bookinginfo[0].balancedue
+      let currency = this.$store.state.bookinginfo.bookinginfo[0].currencyname
+      let resref = this.$store.state.bookinginfo.bookinginfo[0].reservationref
+      if (balancedue > 0) {
+        let Host = import.meta.env.VITE_HOST
+        var body = JSON.stringify({
+          amount: balancedue.toFixed(2),
+          currency: currency,
+          resref: resref,
+        })
+        var requestOptions = {
+          method: 'POST',
+          body: body,
+          redirect: 'follow',
+        }
+        fetch(Host + '/.netlify/functions/reqtrans', requestOptions)
+          .then((response) => response.text())
+          .then((result) => {
+            let res = JSON.parse(result).Request.URI._text
+            this.payurl = res
+            this.loading = false
+          })
+          .catch((error) => {
+            console.log('request transaction failed: ', error)
+          })
+      } else {
+        this.$router.push({
+          path: '/summary?pymnt=nobal',
+          query: {
+            pymnt: 'nobal',
+          },
+        })
+      }
     },
   },
 }
