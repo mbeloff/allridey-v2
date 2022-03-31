0212a46bce5c8390cd48e7a99068f795fa81c86b
michael beloff <michael.a.beloff@gmail.com>
2022-03-31 12:32:57 +1000

    Update 404 message

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,11 +2,11 @@
   <div
     class="h-full w-full grid place-items-center bg-gradient-to-b from-blue-50"
   >
-    <div class="text-left">
-      <p class="text-7xl text-gray-400 opacity-20 font-black">404</p>
-      <p class="text-2xl text-gray-400 font-black mb-4">Page Not Found</p>
+    <div class="text-left px-2">
+      <p class="text-7xl text-gray-400 opacity-20 font-black">404 Not Found</p>
+      <p class="text-2xl text-gray-400 font-black mb-4">I think we got lost</p>
       <router-link :to="{ path: '/' }" class="text-blue-600 hover:underline"
-        >return home <i class="far fa-home"></i
+        >take me home <i class="far fa-home"></i
       ></router-link>
     </div>
   </div>
