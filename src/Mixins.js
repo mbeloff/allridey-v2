00c0725b2ef9632a5cffdc3d95639c54c6609b47
michael beloff <michael.a.beloff@gmail.com>
2022-08-17 15:35:17 +1000

    remove code for post-booking api

--- src/Mixins.js
+++ src/Mixins.js
@@ -1,4 +1,3 @@
-import store from './store.js'
 export default {
   methods: {
     signRequest(method) {
@@ -45,49 +44,6 @@ export default {
         .catch((error) => {
           console.log(JSON.parse(method).method, error)
           return error
-        })
-    },
-
-    getToken() {
-      let Host = import.meta.env.VITE_HOST
-      var requestOptions = {
-        method: 'POST',
-        redirect: 'follow',
-      }
-      fetch(Host + '/.netlify/functions/getToken', requestOptions)
-        .then((response) => response.text())
-        .then((result) => {
-          const res = JSON.parse(result)
-          store.dispatch('token', res.access_token)
-          store.dispatch('tokenExp', res['.expires'])
-        })
-        .catch((error) => console.log('error', error))
-      // return store.state.token
-    },
-
-    async postapiCall(method) {
-      let token = store.state.token
-      let expires = new Date(store.state.tokenExp).getTime()
-      let now = new Date().getTime()
-      if (expires < now) {
-        console.log('refreshing token')
-        this.getToken()
-      }
-      var requestOptions = {
-        headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify(method),
-      }
-      return await fetch(
-        'https://api.rentalcarmanager.com/v32/api',
-        requestOptions
-      )
-        .then((response) => response.text())
-        .then((result) => {
-          return JSON.parse(result)
         })
     },
   },
